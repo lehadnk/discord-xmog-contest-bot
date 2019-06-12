1433e2c1018d8ced63eb4bfe759abde2f8b34445
Alexey Zauzin <lehadnk@gmail.com>
2019-06-12 23:09:22 +0400

    Added unit tests for validators

---
+++ test/AddParticipantMessageValidator.test.ts
@@ -0,0 +1,89 @@
+import {expect} from "chai";
+import {DiscordMessage} from "../src/DTO/DiscordMessage";
+import {AddParticipantMessageValidator} from "../src/Validators/AddParticipantMessageValidator";
+
+describe('Tests VoteForParticipantMessageValidator', () => {
+    it('should validate correct message', () => {
+        let msg = new DiscordMessage(
+            '208939653426839552',
+            '512034124935426920',
+            '120359014053436256',
+            'Энмеркар - Азурегос',
+            ['http://google.com/123.jpg']
+        );
+        let result = AddParticipantMessageValidator.validate(msg);
+        expect(result.isValid).to.be.true;
+    });
+
+    it('should invalidate empty message', () => {
+        let msg = new DiscordMessage(
+            '208939653426839552',
+            '512034124935426920',
+            '120359014053436256',
+            '',
+            ['http://google.com/123.jpg']
+        );
+        let result = AddParticipantMessageValidator.validate(msg);
+        expect(result.isValid).to.be.false;
+    });
+
+    it('should invalidate message with no image', () => {
+        let msg = new DiscordMessage(
+            '208939653426839552',
+            '512034124935426920',
+            '120359014053436256',
+            'Энмеркар - Азурегос',
+            []
+        );
+        let result = AddParticipantMessageValidator.validate(msg);
+        expect(result.isValid).to.be.false;
+    });
+
+    it('should invalidate message with no character name', () => {
+        let msg = new DiscordMessage(
+            '208939653426839552',
+            '512034124935426920',
+            '120359014053436256',
+            ' - Азурегос',
+            ['http://google.com/123.jpg']
+        );
+        let result = AddParticipantMessageValidator.validate(msg);
+        expect(result.isValid).to.be.false;
+    });
+
+    it('should invalidate message with no realm mane', () => {
+        let msg = new DiscordMessage(
+            '208939653426839552',
+            '512034124935426920',
+            '120359014053436256',
+            'Энмеркар - ',
+            ['http://google.com/123.jpg']
+        );
+        let result = AddParticipantMessageValidator.validate(msg);
+        expect(result.isValid).to.be.false;
+    });
+
+    it('should invalidate malformed message', () => {
+        let msg = new DiscordMessage(
+            '208939653426839552',
+            '512034124935426920',
+            '120359014053436256',
+            'Энмеркар азурегосч',
+            ['http://google.com/123.jpg']
+        );
+        let result = AddParticipantMessageValidator.validate(msg);
+        expect(result.isValid).to.be.false;
+    });
+
+    it('should validate message with pass description', () => {
+        let msg = new DiscordMessage(
+            '208939653426839552',
+            '512034124935426920',
+            '120359014053436256',
+            'Энмеркар - Азурегос/nMy description',
+            ['http://google.com/123.jpg']
+        );
+        let result = AddParticipantMessageValidator.validate(msg);
+        expect(result.isValid).to.be.true;
+    });
+});
---
+++ test/VoteForParticipantMessageValidator.test.ts
@@ -0,0 +1,77 @@
+import {expect} from "chai";
+import {VoteForParticipantMessageValidator} from "../src/Validators/VoteForParticipantMessageValidator";
+import {DiscordMessage} from "../src/DTO/DiscordMessage";
+
+describe('Tests VoteForParticipantMessageValidator', () => {
+    it('should validate correct message', () => {
+        let msg = new DiscordMessage(
+            '208939653426839552',
+            '512034124935426920',
+            '120359014053436256',
+            '/vote Селанаар - Азурегос',
+            []
+        );
+        let result = VoteForParticipantMessageValidator.validate(msg);
+        expect(result.isValid).to.be.true;
+    });
+
+    it('should invalidate message with no /vote tag', () => {
+        let msg = new DiscordMessage(
+            '208939653426839552',
+            '512034124935426920',
+            '120359014053436256',
+            'Селанаар - Азурегос',
+            []
+        );
+        let result = VoteForParticipantMessageValidator.validate(msg);
+        expect(result.isValid).to.be.false;
+    });
+
+    it('should invalidate message with no name', () => {
+        let msg = new DiscordMessage(
+            '208939653426839552',
+            '512034124935426920',
+            '120359014053436256',
+            '/vote - Азурегос',
+            []
+        );
+        let result = VoteForParticipantMessageValidator.validate(msg);
+        expect(result.isValid).to.be.false;
+    });
+
+    it('should invalidate message with no realm', () => {
+        let msg = new DiscordMessage(
+            '208939653426839552',
+            '512034124935426920',
+            '120359014053436256',
+            '/vote Селанаар - ',
+            []
+        );
+        let result = VoteForParticipantMessageValidator.validate(msg);
+        expect(result.isValid).to.be.false;
+    });
+
+    it('should invalidate message with incorrect character name', () => {
+        let msg = new DiscordMessage(
+            '208939653426839552',
+            '512034124935426920',
+            '120359014053436256',
+            '/vote Эн - Азурегос',
+            []
+        );
+        let result = VoteForParticipantMessageValidator.validate(msg);
+        expect(result.isValid).to.be.false;
+    });
+
+    it('should invalidate message with incorrect realm name', () => {
+        let msg = new DiscordMessage(
+            '208939653426839552',
+            '512034124935426920',
+            '120359014053436256',
+            '/vote Энмеркар - Азу',
+            []
+        );
+        let result = VoteForParticipantMessageValidator.validate(msg);
+        expect(result.isValid).to.be.false;
+    });
+});
