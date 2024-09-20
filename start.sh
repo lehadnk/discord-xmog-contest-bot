62119921ae16e74a24f19714a7c1b2175334e44a
Alexey Hromov <bratpidd2012@gmail.com>
2024-09-20 20:15:13 +0300

    supervisord possible fix4

--- start.sh
+++ start.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-
-# Navigate to the app directory
 cd /usr/src/app
-
-# Install dependencies
 npm install
-
-# Compile the project
 npm run compile
-
-# Start supervisord
 exec /usr/bin/supervisord
